501cc85abd918c49f1e2664330a9105d066b4aea
alesia-cameron <aecam@uw.edu>
2025-12-30 13:10:19 -0800

    Update movement_detector.sv

--- movement_detector.sv
+++ movement_detector.sv
@@ -12,12 +12,10 @@ coordinates every cycle.
 Sets moved output high whenever either horizontal or vertical position 
 differs from last cycle. Used by game controller to catch player cheating by 
 moving during red light
-
 */
 
 module movement_detector (
-  
-   input logic clk,
+    input logic clk,
     input logic reset,
     input logic [9:0] current_x, //current horizontal position of player car
     input logic [8:0] current_y, //current vertical position of player car
@@ -47,6 +45,6 @@ module movement_detector (
     
     //moved goes high if either x or y coordinate changed
     //catches horizontal movement, vertical movement, or diagonal movement
-	 assign moved = (current_x != prev_x) || (current_y != prev_y);
-	 
+	assign moved = (current_x != prev_x) || (current_y != prev_y);
+
 endmodule
