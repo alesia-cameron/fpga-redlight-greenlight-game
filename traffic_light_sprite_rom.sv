d9e70cbe046850acebb81f440ed4d2021fcf4b9b
alesia-cameron <aecam@uw.edu>
2025-12-30 13:07:09 -0800

    Update traffic_light_sprite_rom.sv

--- traffic_light_sprite_rom.sv
+++ traffic_light_sprite_rom.sv
@@ -8,6 +8,7 @@ Outputs:  pixel
 
 Description: traffic Light Sprite ROM 40x40 pixel
  */
+
 module traffic_light_sprite_rom (
     input logic [5:0] sprite_x,  // 0-39 (40 pixels wide)
     input logic [5:0] sprite_y,  // 0-39 (40 pixels tall)
